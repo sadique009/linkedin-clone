6b1ad7b149e25e871a711aec2bcb141804963766
msn009 <sadique.nasim786@gmail.com>
2023-04-02 16:44:41 +0530

    made changes

---
+++ src/Components/LikeDislike.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const LikeDislike = () => {
+  return (
+    <View>
+      <Text>LikeDislike</Text>
+    </View>
+  );
+};
+
+export default LikeDislike;
---
+++ src/Components/Premium.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Premium = () => {
+  return (
+    <View>
+      <Text>Premium</Text>
+    </View>
+  );
+};
+
+export default Premium;
